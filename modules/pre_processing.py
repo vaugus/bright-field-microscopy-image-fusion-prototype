091f24d6db3a81a2ea965bb992f8846bf930ca5b
vaugusto92 <vaugusto92@outlook.com>
2020-01-23 19:40:44 -0300

    First implementation of PreProcessing class and FocusMeasures class

---
+++ modules/pre_processing.py
@@ -0,0 +1,103 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""This module's docstring summary line.
+
+This is a multi-line docstring. Paragraphs are separated with blank lines.
+Lines conform to 79-column limit.
+
+Module and packages names should be short, lower_case_with_underscores.
+Notice that this in not PEP8-cheatsheet.py
+
+Seriously, use flake8. Atom.io with https://atom.io/packages/linter-flake8
+is awesome!
+
+See http://www.python.org/dev/peps/pep-0008/ for more PEP-8 details
+"""
+
+import os
+
+import numpy as np
+from PIL import Image, ImageFilter, ImageOps
+import skimage
+
+
+class PreProcessing(object):
+    """First line of a docstring is short and next to the quotes.
+
+    Class and exception names are CapWords.
+
+    Closing quotes are on their own line
+    """
+
+    def __init__(self):
+        pass
+
+    def open_image(self, path):
+        """Method and function names are lower_case_with_underscores.
+
+        Always use self as first arg.
+        """
+        img = Image.open(path)
+        return img
+
+
+    def sort_dataset_names(self, files):
+        files = [item.replace('.jpg', '') for item in files]
+        files.sort()
+        files = [item + '.jpg' for item in files]
+        
+        return files
+
+    def open_dataset(self, path):
+        """Method and function names are lower_case_with_underscores.
+
+        Always use self as first arg.
+        """
+        files = sort_dataset_names(os.listdir(path))
+        return [open_image(path + img) for img in files]
+
+
+    def normalize_image(self, arr):
+        """Method and function names are lower_case_with_underscores.
+
+        Always use self as first arg.
+        """
+        min_, max_ = arr.min(), arr.max()
+        return (arr - min_) / (max_ - min_)
+
+
+    def image_to_ndarray(self, img):
+        """Method and function names are lower_case_with_underscores.
+
+        Always use self as first arg.
+        """
+        return np.array(img, dtype=np.float32) / 255.
+
+
+    def ndarray_to_image(self, arr, normalize=True):
+        """Method and function names are lower_case_with_underscores.
+
+        Always use self as first arg.
+        """
+        if normalize:
+            arr = normalize_image(arr) 
+        
+        return Image.fromarray((arr * 255.).astype(np.uint8))
+
+
+    def grayscale_averaging(self, img):
+        """Method and function names are lower_case_with_underscores.
+
+        Always use self as first arg.
+        """
+        gray = image_to_array(img).mean(axis=2)
+        return ndarray_to_image(gray)
+
+
+    def grayscale_gleam(img):
+        """Method and function names are lower_case_with_underscores.
+
+        Always use self as first arg.
+        """
+        gray = np.power(image_to_array(img), 1/2.2).mean(axis=2)
+        return ndarray_to_image(gray)
