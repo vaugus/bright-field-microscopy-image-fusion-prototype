01246923ab3206bb520a287be4d6f15a5230af89
Victor Augusto <vaugusto@alumni.usp.br>
2023-04-26 20:47:13 -0300

    refactor: cleanup documentation

--- README.md
+++ README.md
@@ -1,19 +1,31 @@
 # Bright-Field Microscopy Image Fusion Prototype
 
-Python implementation of a Laplacian of Gaussian Energy based image fusion procedure.
+[![Continuous Integration](https://github.com/vaugus/bright-field-microscopy-image-fusion-prototype/actions/workflows/ci.yaml/badge.svg)](https://github.com/vaugus/bright-field-microscopy-image-fusion-prototype/actions/workflows/ci.yaml)
 
-### Prerequisites
+A Python implementation of an image fusion method using the Laplacian of Gaussian Energy approach.
 
-requirements.txt
+## Fusion Rule
 
-### Running
+The first step of the fusion rule is to extract the edges of the images with the Laplacian filter. Then, the indices for the highest energies of laplacian are calculated for each pixel and associated to one of the images in the stack. The final image is composed by choosing pixels from the layers with highest energy values.
 
-python3 main.py > (input file with a string as the path to the dataset)
+## Evaluation of fusion results
 
-## Built With
+We used the following evaluation methods, as proposed by ([NAIDU; RAOL, 2008](#references)): the spatial frequency index, the standard deviation of the histogram of the image, and the image entropy.
 
-* [Python](https://www.python.org/)
-* [NumPy](https://numpy.org/)
-* [SciPy](https://www.scipy.org/)
-* [scikit-image](https://scikit-image.org/)
-* [Pillow](https://pillow.readthedocs.io/en/stable/)
+## Usage
+
+```sh
+python3 main.py < /path/to/image/dataset
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## References
+
+Naidu, V. P. S., and Jitendra R. Raol. "Pixel-level image fusion using wavelets and principal component analysis." Defence Science Journal 58.3 (2008): 338.
--- modules/fusion_rules.py
+++ modules/fusion_rules.py
@@ -1,12 +1,5 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Module with fusion rule implementations.
-
-This module contains an abstract class to organize any
-fusion rule implementation. It also contains a class
-that implements the Energy of Laplacian of Gaussian
-fusion approach.
-"""
 
 import abc
 import os
@@ -16,54 +9,26 @@ from scipy import ndimage
 
 
 class AbstractFocusMeasure(metaclass=abc.ABCMeta):
-    """Abstract class for fusion rule implementations."""
-
     def __init__(self):
-        """Constructor."""
         super(AbstractFocusMeasure, self).__init__()
 
     @abc.abstractmethod
     def execute(self, images, grayscale_images, sigma):
-        """Executes the fusion rule."""
         pass
 
 
 class LaplacianOfGaussianEnergy(AbstractFocusMeasure):
-    """Class that implements the Laplacian of Gaussian Energy fusion rule."""
 
     def __init__(self):
-        """Constructor."""
         super().__init__()
 
     def laplacian(self, image):
         return ndimage.gaussian_laplace(image, sigma=0.7)
 
     def execute(self, dataset, grayscale_images):
-        """Executes the Energy of Laplacian of Gaussian fusion rule.
-
-        :param kwargs['dataset']: List of dataset images.
-        :type kwargs['dataset']: list of PIL.Image
-
-        :param kwargs['gray_dataset']: List of grayscale numpy ndarray
-        representations of dataset images.
-        :type kwargs['gray_dataset']: list of numpy.ndarray
-
-        :param kwargs['sigma']: Standard deviation of the Gaussian function.
-        :type kwargs['sigma']: float
-
-        :returns: The fused image.
-        :rtype: numpy.ndarray
-        """
-        # compute the edges of the images with the laplacian filter
         edges = np.array([self.laplacian(img) for img in grayscale_images])
 
-        # compute 3D indices of the highest energies of laplacian,
-        # pixel-wise
-
         energies = np.argmax(edges, axis=0)
-
-        # Synthesize an image by sampling from each layer
-        # according to it's highest energy
 
         height, width = grayscale_images[0].shape
 
